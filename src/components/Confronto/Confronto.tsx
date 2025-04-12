76c70a0ffcbf205f8889b3e17249ac9ede9d68c9
Your Name <franciscogrynberg34@gmail.com>
2025-04-12 12:41:15 -0300

    v1.0.8 CSS changes in mobile version

--- src/components/Confronto/Confronto.tsx
+++ src/components/Confronto/Confronto.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
 import Menu from '@/components/Confronto/Menu';
 import Info from '@/components/Confronto/Info';
 import Partidas from '@/components/Confronto/Partidas';
 
 export default function Confronto() {
-
-  const partidas = useSelector((state: RootState) => state.partidas);
-
-  console.log(partidas);
 
   return (
     <div className="confronto">
